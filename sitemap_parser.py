4293cdd23e44296609f14a5316f7bce1a117bf76
Kevin <kevin.tarvin@gmail.com>
2016-11-14 16:37:14 -0500

    Refactored main to output csv

--- sitemap_parser.py
+++ sitemap_parser.py
@@ -1,37 +1,58 @@
 # -*- coding: utf-8 -*-
 """
 File output by Eric Watson (https://github.com/Edubya77)
+Authors: Kevin Tarvin | Eric Watson
 """
 
-from bs4 import BeautifulSoup
+import sys
+import urllib
 import requests
+import tldextract
+import pandas as pd
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
-outfile = 'output.txt'
-ofile = open(outfile,"w")
-loc = []
 
+def nameParser(url):
+    '''Parse a URL and return the domain name as a string'''
+
+    if isinstance(url, str):
+        client = tldextract.extract(url)
+        return str(client.domain)
+    else:
+        return str("Invalid client name")
 
 def fetchsite(url):
     '''Creates a BeautifulSoup object from the input URL'''
+    
     r = requests.get(url)
-    soup = BeautifulSoup(r.text)
+    soup = BeautifulSoup(r.text, 'html5lib')
     return soup
 
-def main():
-    soup = fetchsite("http://zh.rebeccaminkoff.com/sitemaps/www.rebeccaminkoff.com/sitemap.xml")
+def main(argv):
+	'''Parse an xml sitemap and return a csv with percent encoded URLs'''
+
+    soup = fetchsite(argv[0])
     urls = soup.findAll('url')
     if not urls:
-        return False
+        raise SystemExit("No Urls in Sitemap Input")
     
     out = []
     
     for url in urls:
         loc = url.find('loc').string
-        out.append(loc.encode("utf8"))
+        parsed_url = urlparse(loc)
+        path = parsed_url.path
+        encoded_url = "%s://%s%s" % (parsed_url.scheme, parsed_url.netloc, urllib.parse.quote(path))
+        out.append(encoded_url)
+    data = {"URLs": out}
+    df = pd.DataFrame(data)
+    fname = nameParser(out[0])
+    df.to_csv("%s.csv" % (fname))
         
-    for i in out:
-        print(i)
-        ofile.write(i + "\n")    
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    try:
+    	main(sys.argv[1:])
+    except Exception as e:
+    	raise SystemExit(e)
